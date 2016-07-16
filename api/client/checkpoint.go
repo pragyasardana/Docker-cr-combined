99a5c18a88b18663f4d9dcfe4d1b2bb620bc223b
System Administrator <root@peters-MacBook-Pro.local>
2016-07-16 14:54:49 -0400

    updated for RPC

--- api/client/checkpoint.go
+++ api/client/checkpoint.go
@@ -4,7 +4,6 @@ package client
 
 import (
 	"fmt"
-
 	"github.com/docker/docker/api/types"
 	Cli "github.com/docker/docker/cli"
 	flag "github.com/docker/docker/pkg/mflag"
@@ -22,9 +21,12 @@ func (cli *DockerCli) CmdCheckpoint(args ...string) error {
 		flWorkDir      = cmd.String([]string{"-work-dir"}, "", "directory for storing log file")
 		flLeaveRunning = cmd.Bool([]string{"-leave-running"}, false, "leave the container running after checkpoint")
                 flPrevImgDir      = cmd.String([]string{"-prev-image-dir"}, "", "previous Image directory")  //peter
-		flTrackMem = cmd.Bool([]string{"-track-mem"}, false, "track memory flag")   // peter
-                flEnablePreDump = cmd.Bool([]string{"-predump"}, false, "predump flag")   // peter 
-                flAutoDedup = cmd.Bool([]string{"-auto-dedup"}, false, "auto-dedup flag")   // peter 
+		flTrackMem = cmd.Bool([]string{"-track-mem"}, false, "Enable track memory flag")   // peter
+                flEnablePreDump = cmd.Bool([]string{"-predump"}, false, "Enable predump flag")   // peter 
+                flAutoDedup = cmd.Bool([]string{"-auto-dedup"}, false, "Enable auto-dedup flag")   // peter 
+                flPageServer = cmd.Bool([]string{"-page-server"}, false, "Enable Page-Server flag for RPC")   // peter 
+                flAddress       = cmd.String([]string{"-address"}, "", "IP address of Page-Server used with Page-Server flag")    //peter 
+                flPort      = cmd.Int([]string{"-port"},0 , "Port of Page-Server used with Page-Server flag")  
 	)
 
 	if err := cmd.ParseFlags(args, true); err != nil {
@@ -36,6 +38,27 @@ func (cli *DockerCli) CmdCheckpoint(args ...string) error {
 		return nil
 	}
 
+      
+
+        // add by peter
+        var p int32
+        var tmp int = *flPort;
+         fmt.Printf("flPageServer:[%v], flAddress:[%s], flPort:[%v] \n", *flPageServer, *flAddress, tmp )  // peter
+
+        if  *flPageServer == true {
+              if *flAddress =="" {
+                   return fmt.Errorf("Page Server enabled but address is not assigned");
+              }
+              if *flPort ==0 {
+                  return fmt.Errorf("Page Server enabled but port is not assigned");
+              } 
+              
+              p = int32(tmp)
+        }
+
+
+
+
 	criuOpts := types.CriuConfig{
 		ImagesDirectory: *flImgDir,
 		WorkDirectory:   *flWorkDir,
@@ -44,6 +67,9 @@ func (cli *DockerCli) CmdCheckpoint(args ...string) error {
                 TrackMemory:  *flTrackMem,  //peter
                 EnablePreDump:  *flEnablePreDump, //peter
                 AutoDedup:      *flAutoDedup,     //peter 
+                PageServer:     *flPageServer,    //peter 
+                Address:        *flAddress,       //peter 
+                Port:           p,         //peter 
 	}
 
 	var encounteredError error
